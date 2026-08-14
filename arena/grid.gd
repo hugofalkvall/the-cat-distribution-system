58c080f5e2e8c678d889fabd8416e749c6cdf6da
bAjzk1234 <53091747+bAjzk1234@users.noreply.github.com>
2026-08-14 13:16:43 +0200

    Grid script

--- arena/grid.gd
+++ arena/grid.gd
@@ -3,21 +3,21 @@ extends Node2D
 const CELL_SIZE := 16
 const GRID_WIDTH := 24
 const GRID_HEIGHT := 20
+const GRID_ORIGIN := Vector2(128, 32)
 
 var occupied_cells: Dictionary = {}
 
 # Translate grid values to real engine x,y pos
 func grid_to_world(cell: Vector2i) -> Vector2:
-	return Vector2(
-		cell.x * CELL_SIZE,
-		cell.y * CELL_SIZE
-	)
+	return GRID_ORIGIN + Vector2(cell) * CELL_SIZE
 
 # Translate real engine x,y values to grid pos
 func world_to_grid(position: Vector2) -> Vector2i:
+	var local_position := position - GRID_ORIGIN
+
 	return Vector2i(
-		floor(position.x / CELL_SIZE),
-		floor(position.y / CELL_SIZE)
+		floor(local_position.x / CELL_SIZE),
+		floor(local_position.y / CELL_SIZE)
 	)
 
 # Check if element exsists in current x,y pos 
